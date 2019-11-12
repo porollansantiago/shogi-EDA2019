82edd82d0c3404af33d67a7bb4fbce5ef57c5f3a
Santi Porollan <santiporollan@gmail.com>
2019-11-12 17:29:10 -0300

    fixed copy coords

--- board_objects.py
+++ board_objects.py
@@ -2,7 +2,7 @@
 
 class Board_objects:
     def __init__(self, coord_dictionary):
-        self.coords = coord_dictionary
+        self.coords = coord_dictionary.copy()
 
     def add_to_board(self, board):
         for piece in self.coords.keys():
