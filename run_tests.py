5ed60575e141db85b4fb442c60fe1fbb665cbe0c
Santi Porollan <santiporollan@gmail.com>
2019-11-12 14:25:19 -0300

    created run tests

---
+++ run_tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+modules_to_test = (
+    "test_board",
+    "test_api",
+    "test_board_objects"
+)
+
+
+def suite():
+    tests = unittest.TestSuite()
+    for module in map(__import__, modules_to_test):
+        tests.addTest(unittest.findTestCases(module))
+    return tests
+
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(verbosity=2).run(suite())
