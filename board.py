04a6e4f8e28fb414eba01f470d07b31a91c00abd
Santi Porollan <santiporollan@gmail.com>
2019-11-12 13:33:23 -0300

    added function make board

--- board.py
+++ board.py
@@ -5,3 +5,13 @@ class Board:
     def __init__(self):
         self.black = Board_objects(coords.black)
         self.white = Board_objects(coords.white)
+
+    def make_board(self):
+        board = [["   " for _ in range(9)], ["   " for _ in range(9)],
+                ["   " for _ in range(9)], ["   " for _ in range(9)], 
+                ["   " for _ in range(9)], ["   " for _ in range(9)], 
+                ["   " for _ in range(9)], ["   " for _ in range(9)], 
+                ["   " for _ in range(9)]]
+        board = self.black.add_to_board(board)
+        board = self.white.add_to_board(board)
+        return board
---
+++ test_board.py
@@ -0,0 +1,25 @@
+import unittest
+from parameterized import parameterized
+from board import Board
+
+
+class Test_board(unittest.TestCase):
+    
+    def setUp(self):
+        self.board = Board()
+
+    def test_make_board_original(self):
+        new_board = [['L2 ', 'KN2', 'SG2', 'GG2', ' K ', 'GG1', 'SG1', 'KN1', 'L1 '], 
+                     ['   ', ' R ', '   ', '   ', '   ', '   ', '   ', ' B ', '   '], 
+                     ['P9 ', 'P8 ', 'P7 ', 'P6 ', 'P5 ', 'P4 ', 'P3 ', 'P2 ', 'P1 '], 
+                     ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
+                     ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
+                     ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
+                     ['P1 ', 'P2 ', 'P3 ', 'P4 ', 'P5 ', 'P6 ', 'P7 ', 'P8 ', 'P9 '], 
+                     ['   ', ' B ', '   ', '   ', '   ', '   ', '   ', ' R ', '   '], 
+                     ['L1 ', 'KN1', 'SG1', 'GG1', ' K ', 'GG2', 'SG2', 'KN2', 'L2 ']]
+        self.assertEqual(self.board.make_board(), new_board)
+
+
+if __name__ == "__main__":
+    unittest.main()
