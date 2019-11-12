2022dce99ec51db920183729811428d3987b6996
Santi Porollan <santiporollan@gmail.com>
2019-11-12 13:25:30 -0300

    added function add to board

--- board_objects.py
+++ board_objects.py
@@ -3,3 +3,8 @@
 class Board_objects:
     def __init__(self, coord_dictionary):
         self.coords = coord_dictionary
+
+    def add_to_board(self, board):
+        for piece in self.coords.keys():
+            board[self.coords[piece][1]][self.coords[piece][0]] = piece
+        return board
---
+++ test_board_objects.py
@@ -0,0 +1,42 @@
+import unittest
+import coordinates as coords
+from parameterized import parameterized
+from board_objects import Board_objects
+
+class Test_board_objects(unittest.TestCase):
+    
+    def setUp(self):
+        self.board = [["   " for _ in range(9)], ["   " for _ in range(9)],
+                      ["   " for _ in range(9)], ["   " for _ in range(9)],
+                      ["   " for _ in range(9)], ["   " for _ in range(9)],
+                      ["   " for _ in range(9)], ["   " for _ in range(9)],
+                      ["   " for _ in range(9)]]
+
+    @parameterized.expand([
+        (coords.black, [['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
+                        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
+                        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
+                        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
+                        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
+                        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
+                        ['P1 ', 'P2 ', 'P3 ', 'P4 ', 'P5 ', 'P6 ', 'P7 ', 'P8 ', 'P9 '], 
+                        ['   ', ' B ', '   ', '   ', '   ', '   ', '   ', ' R ', '   '], 
+                        ['L1 ', 'KN1', 'SG1', 'GG1', ' K ', 'GG2', 'SG2', 'KN2', 'L2 ']]),
+
+        (coords.white, [['L2 ', 'KN2', 'SG2', 'GG2', ' K ', 'GG1', 'SG1', 'KN1', 'L1 '], 
+                        ['   ', ' R ', '   ', '   ', '   ', '   ', '   ', ' B ', '   '], 
+                        ['P9 ', 'P8 ', 'P7 ', 'P6 ', 'P5 ', 'P4 ', 'P3 ', 'P2 ', 'P1 '], 
+                        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
+                        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
+                        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
+                        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
+                        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
+                        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ']])
+    ])
+    def test_add_to_board(self, coords, new_board):
+        board_objects = Board_objects(coords)
+        self.assertEqual(board_objects.add_to_board(self.board), new_board)
+
+
+if __name__ == "__main__":
+    unittest.main()
