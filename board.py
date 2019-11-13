864ed6fc649d55ca5f3ffb3bfedc19751aef971e
Santi Porollan <santiporollan@gmail.com>
2019-11-13 18:01:48 -0300

    move using coordinates

--- board.py
+++ board.py
@@ -8,6 +8,7 @@ class Board:
         self.black = Board_objects(coords.black)
         self.white = Board_objects(coords.white)
         self.turn = "black"
+        self.piece = ""
         self.moves = Moves()
 
     def make_board(self):
@@ -20,12 +21,20 @@ class Board:
         board = self.white.add_to_board(board)
         return board
 
-    def play(self, piece, new_coords):
-        if self.moves.validate(self.turn, piece, new_coords, self.black, self.white):
+    def play(self, new_coords):
+        if self.turn == "black":
+            if not self.piece:
+                self.piece = self.black.get_piece(new_coords)
+                return
+        if self.turn == "white":
+            if not self.piece:
+                self.piece = self.white.get_piece(new_coords)
+                return
+        if self.moves.validate(self.turn, self.piece, new_coords, self.black, self.white):
             if self.turn == "black":
-                self.__make_move(piece, new_coords, self.black)
+                self.__make_move(self.piece, new_coords, self.black)
             elif self.turn == "white":
-                self.__make_move(piece, new_coords, self.white)
+                self.__make_move(self.piece, new_coords, self.white)
         return self.make_board()
 
     def __make_move(self, piece, new_coords, player):
--- test_board.py
+++ test_board.py
@@ -21,7 +21,7 @@ class Test_board(unittest.TestCase):
         self.assertEqual(self.board.make_board(), new_board)
 
     @parameterized.expand([
-        ('P1 ', [0, 5], [['L2 ', 'KN2', 'SG2', 'GG2', ' K ', 'GG1', 'SG1', 'KN1', 'L1 '], 
+        ([0, 6], [0, 5], [['L2 ', 'KN2', 'SG2', 'GG2', ' K ', 'GG1', 'SG1', 'KN1', 'L1 '], 
                          ['   ', ' R ', '   ', '   ', '   ', '   ', '   ', ' B ', '   '], 
                          ['P9 ', 'P8 ', 'P7 ', 'P6 ', 'P5 ', 'P4 ', 'P3 ', 'P2 ', 'P1 '], 
                          ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
@@ -31,7 +31,7 @@ class Test_board(unittest.TestCase):
                          ['   ', ' B ', '   ', '   ', '   ', '   ', '   ', ' R ', '   '], 
                          ['L1 ', 'KN1', 'SG1', 'GG1', ' K ', 'GG2', 'SG2', 'KN2', 'L2 ']]),
 
-        ('P9 ', [8, 5], [['L2 ', 'KN2', 'SG2', 'GG2', ' K ', 'GG1', 'SG1', 'KN1', 'L1 '], 
+        ([8, 6], [8, 5], [['L2 ', 'KN2', 'SG2', 'GG2', ' K ', 'GG1', 'SG1', 'KN1', 'L1 '], 
                          ['   ', ' R ', '   ', '   ', '   ', '   ', '   ', ' B ', '   '], 
                          ['P9 ', 'P8 ', 'P7 ', 'P6 ', 'P5 ', 'P4 ', 'P3 ', 'P2 ', 'P1 '], 
                          ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '], 
@@ -41,8 +41,9 @@ class Test_board(unittest.TestCase):
                          ['   ', ' B ', '   ', '   ', '   ', '   ', '   ', ' R ', '   '], 
                          ['L1 ', 'KN1', 'SG1', 'GG1', ' K ', 'GG2', 'SG2', 'KN2', 'L2 ']])
     ])
-    def test_play(self, piece, new_coords, new_board):
-        self.assertEqual(self.board.play(piece, new_coords), new_board)
+    def test_play(self, piece_coords, new_coords, new_board):
+        self.board.play(piece_coords)
+        self.assertEqual(self.board.play(new_coords), new_board)
 
 if __name__ == "__main__":
     unittest.main()
