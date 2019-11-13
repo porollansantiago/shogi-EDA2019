c757e14954d67d672cce13a9629b096be22b9e26
Santi Porollan <santiporollan@gmail.com>
2019-11-13 16:33:37 -0300

    added lance moves

--- moves.py
+++ moves.py
@@ -6,17 +6,17 @@ class Moves():
 
     def validate(self, turn, piece_to_move, new_coords, black, white):
         if turn == "black":
-            if self.validate_player(turn, piece_to_move, new_coords, black):
+            if self.validate_player(turn, piece_to_move, new_coords, black, white):
                 return False
         elif turn == "white":
-            if self.validate_player(turn, piece_to_move, new_coords, white):
+            if self.validate_player(turn, piece_to_move, new_coords, white, black):
                 return False
         return True
 
-    def validate_player(self, turn, piece_to_move, new_coords, player):
+    def validate_player(self, turn, piece_to_move, new_coords, player, other_player):
         if self.__overlapping(new_coords, player):
             return True
-        if new_coords not in self.get_move_array(turn, piece_to_move, new_coords, player):
+        if new_coords not in self.get_move_array(turn, piece_to_move, new_coords, player, other_player):
             return True
         return False
 
@@ -26,11 +26,26 @@ class Moves():
                 return True
         return False
 
-    def get_move_array(self, turn, piece_to_move, new_coords, player):
+    def get_move_array(self, turn, piece_to_move, new_coords, player, other_player):
         move_array = []
-        if turn == "black" and "P" in piece_to_move:
-            move_array.append([player.coords[piece_to_move][0], player.coords[piece_to_move][1] - 1])
-        elif turn == "white" and "P" in piece_to_move:
-            move_array.append([player.coords[piece_to_move][0], player.coords[piece_to_move][1] + 1])
+        if turn == "black":
+            if "P" in piece_to_move:
+                move_array.append([player.coords[piece_to_move][0], player.coords[piece_to_move][1] - 1])
+            elif "L" in piece_to_move:
+                end, step = -1, -1
+                move_array = self.get_lance_moves(piece_to_move, move_array, end, step, player, other_player)
+        elif turn == "white":
+            if "P" in piece_to_move:
+                move_array.append([player.coords[piece_to_move][0], player.coords[piece_to_move][1] + 1])
+            elif "L" in piece_to_move:
+                end, step = 9, 1
+                move_array = self.get_lance_moves(piece_to_move, move_array, end, step, player, other_player)
+        return move_array
 
+
+    def get_lance_moves(self, piece, move_array, end, step, player, other_player):
+        for val in range(player.coords[piece][1], end, step):
+            if [player.coords[piece][0], player.coords[piece][1] + val] not in other_player.coords.items():
+                move_array.append([player.coords[piece][0], val])
+        print(move_array)
         return move_array
