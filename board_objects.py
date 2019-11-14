f541ed575ec3d7024d3328370a724d1e47084450
Santi Porollan <santiporollan@gmail.com>
2019-11-14 11:16:59 -0300

    added get coords func

--- board_objects.py
+++ board_objects.py
@@ -15,5 +15,11 @@ class Board_objects:
                 return piece
         return None
 
+    def get_coords(self, piece, x=0, y=0, replace_x=False, replace_y=False):
+        new_coords = [0, 0]
+        new_coords[0] = x if replace_x else self.coords[piece][0] + x
+        new_coords[1] = y if replace_y else self.coords[piece][1] + y
+        return new_coords
+
     def move(self, piece_to_move, x, y):
         self.coords[piece_to_move] = [x, y]
