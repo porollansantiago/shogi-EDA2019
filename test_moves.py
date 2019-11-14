a90d2997b5f9ac00f51143dc11bd98c9d2f38337
Santi Porollan <santiporollan@gmail.com>
2019-11-14 15:48:25 -0300

    removed overlapping tests

--- test_moves.py
+++ test_moves.py
@@ -10,31 +10,6 @@ class test_moves(unittest.TestCase):
         self.black = Board_objects(c.black)
         self.white = Board_objects(c.white)
 
-    @parameterized.expand([
-        ("black", "P1 ", [0, 5]),
-        ("black", "P2 ", [1, 5]),
-        ("black", "P3 ", [2, 5]),
-        ("black", "P4 ", [3, 5]),
-        ("black", "P5 ", [4, 5])
-    ])
-    def test_validate_not_overlapping(self, turn, piece, new_coords):
-        self.assertTrue(self.moves.validate(turn, piece, new_coords, self.black, self.white))
-
-    @parameterized.expand([
-        ("white", "P1 ", [0, 0]),
-        ("white", "P2 ", [1, 0]),
-        ("white", "P3 ", [2, 0]),
-        ("white", "P4 ", [3, 0]),
-        ("white", "P5 ", [4, 0]),
-        ("black", "P1 ", [0, 8]),
-        ("black", "P2 ", [1, 8]),
-        ("black", "P3 ", [2, 8]),
-        ("black", "P4 ", [3, 8]),
-        ("black", "P5 ", [4, 8])
-    ])
-    def test_validate_overlapping(self, turn, piece, new_coords):
-        self.assertFalse(self.moves.validate(turn, piece, new_coords, self.black, self.white))
-
 # PAWN
 
     @parameterized.expand([
@@ -301,6 +276,8 @@ class test_moves(unittest.TestCase):
         ("white", "R1 ", [6, 4], {}, {"R1 ": [4, 4]}),
         ("white", "R1 ", [7, 4], {}, {"R1 ": [4, 4]}),
         ("white", "R1 ", [8, 4], {}, {"R1 ": [4, 4]}),
+        ("black", "R1 ", [0, 4], {"R1 ": [4, 4]}, {"P1": [0, 4]}),
+        ("white", "R1 ", [0, 4], {"P1": [0, 4]}, {"R1 ": [4, 4]}),
     ])
     def test_rook_move(self, turn, piece, new_coords, example_black_coords, example_white_coords):
         example_black = Board_objects(example_black_coords)
@@ -352,6 +329,8 @@ class test_moves(unittest.TestCase):
         ("black", "R1 ", [4, 8], {"R1 ": [4, 4], "P1 ": [4, 6]}, {}),
         ("black", "R1 ", [0, 4], {"R1 ": [4, 4], "P1 ": [2, 4]}, {}),
         ("black", "R1 ", [7, 4], {"R1 ": [4, 4], "P1 ": [6, 4]}, {}),
+        ("black", "R1 ", [0, 4], {"R1 ": [4, 4], "P1 ": [0, 4]}, {}),
+        ("white", "R1 ", [0, 4], {}, {"R1 ": [4, 4], "P1 ": [0, 4]}),
     ])
     def test_rook_move2(self, turn, piece, new_coords, example_black_coords, example_white_coords):
         example_black = Board_objects(example_black_coords)
