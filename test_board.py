48d491e22fea985170eb16635246d509e4cc4bd9
Santi Porollan <santiporollan@gmail.com>
2019-11-15 17:17:46 -0300

    fixed return

--- test_board.py
+++ test_board.py
@@ -108,7 +108,8 @@ class Test_board(unittest.TestCase):
     ])
     def test_play(self, piece_coords, new_coords, new_board):
         self.board.play(piece_coords[0], piece_coords[1])
-        self.assertEqual(self.board.play(new_coords[0], new_coords[1]), new_board)
+        self.board.play(new_coords[0], new_coords[1])
+        self.assertEqual(self.board.make_board(), new_board)
 
     @parameterized.expand([
         ([3, 5], [3, 4], {" P ": [[0, 5],[1, 5],[2, 5], [3, 5]]}, {" P ": [[0, 4], [1, 4], [2, 4], [3, 4]]},
@@ -125,7 +126,8 @@ class Test_board(unittest.TestCase):
     def test_capture(self, coords, new_coords, black, white, new_board):
         board = Board(white, black)
         board.play(coords[0], coords[1])
-        self.assertEqual(board.play(new_coords[0], new_coords[1]), new_board)
+        board.play(new_coords[0], new_coords[1])
+        self.assertEqual(board.make_board(), new_board)
 
 if __name__ == "__main__":
     unittest.main()
