a90633c9e13eb6502e4e005ef52a7a23c4e41667
Santi Porollan <santiporollan@gmail.com>
2019-11-15 20:24:19 -0300

    added move array

--- board.py
+++ board.py
@@ -31,6 +31,7 @@ class Board:
                 except TypeError:
                     return
                 else:
+                    self.move_array = move_array
                     return move_array
         if self.turn == "white":
             if not self.piece:
@@ -40,9 +41,10 @@ class Board:
                 except TypeError:
                     return
                 else:
+                    self.move_array = move_array
                     return move_array
 
-        if self.moves.validate(self.turn, self.piece, self.piece_index, x, y, self.black, self.white):
+        if self.moves.validate(self.turn, self.piece, self.piece_index, x, y, self.black, self.white, self.move_array):
             if self.turn == "black":
                 self.make_move(self.piece, self.piece_index, x, y, self.black, self.white)
             elif self.turn == "white":
@@ -50,6 +52,7 @@ class Board:
         else:
             self.piece = None
             self.piece_index = 0
+            self.move_array = []
 
     def make_move(self, piece, piece_index, x, y, player, opponent):
         player.move(piece, piece_index, x, y)
@@ -57,11 +60,13 @@ class Board:
         self.turn = "white" if self.turn == "black" else "black"
         self.piece = None
         self.piece_index = 0
+        self.move_array = []
     
     def __capture(self, player, opponent, side):
         player_coords = player.get_all_coords()
         for coord in opponent.get_all_coords():
             if coord in player_coords:
-                player.add_piece(opponent.pop(coord), side)
+                piece, idx = opponent.pop(coord)
+                player.add_piece(piece, idx, side)
 
 
