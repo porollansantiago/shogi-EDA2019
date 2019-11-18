4158e23b4f1ce40f20ac931eeda52edb973c9854
porollansantiago <santiporollan@gmail.com>
2019-11-18 17:42:04 -0300

    added settings

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings():
+    def __init__(self):
+        # Screen
+        self.background_color = (100, 100, 100)
+        self.screen_width = 800
+        self.screen_height = 600
+        # Piece
+        self.piece_font_size = 48
+        self.block_size = 50
+        self.board_location_x = 250
+        self.board_location_y = 250
