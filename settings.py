2c49e1fd9cd603a942b38e9cf4b3e4ecc529353c
porollansantiago <santiporollan@gmail.com>
2019-11-18 18:55:13 -0300

    changed font size

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings():
         self.screen_width = 800
         self.screen_height = 600
         # Piece
-        self.piece_font_size = 48
+        self.piece_font_size = 30
         self.block_size = 50
         self.board_location_x = 250
         self.board_location_y = 250
