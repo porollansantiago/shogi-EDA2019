e690b740e6f900cd5d876ea34b65ca85c88bda1b
Santi Porollan <santiporollan@gmail.com>
2019-11-18 11:17:04 -0300

    updated main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from api import Api
 if __name__ == "__main__":
     api_shogi = Api()
     print(api_shogi.get_board())
-    while True:
+    while api_shogi.game_is_running():
         coord = input("coord: ")
         board, move_array = api_shogi.play(coord)
         print(board)
